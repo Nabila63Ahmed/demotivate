572b99a9aa8795e7be486a110cd98fc519964557
Nabila63Ahmed <nabila.ahmed63@gmail.com>
2022-05-05 12:27:35 +0200

    :construction: Adding the viewModel and its dependency

--- app/src/main/java/com/example/demotivate/viewmodel/QuotesViewModel.kt
+++ app/src/main/java/com/example/demotivate/viewmodel/QuotesViewModel.kt
@@ -1,4 +1,23 @@
 package com.example.demotivate.viewmodel
 
-class QuotesViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.demotivate.model.Quote
+import com.example.demotivate.model.QuotesProvider
+
+class QuotesViewModel: ViewModel() {
+
+    private val quoteData = MutableLiveData<Quote>()
+    private val quotes = QuotesProvider().getQuotes()
+
+    fun updateAndGetQuoteData (): LiveData<Quote> {
+        val currentIndex = (0 until quotes.size).random()
+
+        quoteData.value = quotes[currentIndex]
+
+        return quoteData
+    }
+
+    fun randomIndex (size: Int) = (0 until size).random()
 }
