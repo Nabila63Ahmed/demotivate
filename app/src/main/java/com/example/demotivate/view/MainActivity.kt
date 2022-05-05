e98206e0a689f12c97c73ff8869a1feeefe15505
Nabila63Ahmed <nabila.ahmed63@gmail.com>
2022-05-05 12:30:12 +0200

    :construction: Adding the customizing the view components and activity onCreate()

--- app/src/main/java/com/example/demotivate/view/MainActivity.kt
+++ app/src/main/java/com/example/demotivate/view/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.demotivate.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.core.view.isVisible
 import com.example.demotivate.R
+import com.example.demotivate.viewmodel.QuotesViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val model: QuotesViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val demotivateButton: Button = findViewById(R.id.button)
+        demotivateButton.setOnClickListener {
+            val initialTextView: TextView = findViewById(R.id.initialTextView)
+            val quoteTextView: TextView = findViewById(R.id.quoteTextView)
+            val authorTextView: TextView = findViewById(R.id.authorTextView)
+
+            if (initialTextView.isVisible) {
+                initialTextView.visibility = View.INVISIBLE
+                quoteTextView.visibility = View.VISIBLE
+                authorTextView.visibility = View.VISIBLE
+            }
+        }
+
     }
 }
