50f751261270010dc7a4492219cbbc036bd44c67
Nabila63Ahmed <nabila.ahmed63@gmail.com>
2022-05-09 13:21:03 +0200

    :recycle: Use view binding

--- app/src/main/java/com/example/demotivate/view/MainActivity.kt
+++ app/src/main/java/com/example/demotivate/view/MainActivity.kt
@@ -3,50 +3,41 @@ package com.example.demotivate.view
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
 import com.apollographql.apollo3.exception.ApolloException
 import com.example.demotivate.QuotesQuery
-import com.example.demotivate.R
+import com.example.demotivate.databinding.ActivityMainBinding
 import com.example.demotivate.graphql.apolloClient
 import com.example.demotivate.viewmodel.QuotesViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private val model: QuotesViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val demotivateButton: Button = findViewById(R.id.button)
-
-        demotivateButton.isEnabled = false
+        binding.button.isEnabled = false
         getQuotesQuery()
 
-        demotivateButton.setOnClickListener {
-            val initialTextView: TextView = findViewById(R.id.initialTextView)
-            val quoteTextView: TextView = findViewById(R.id.quoteTextView)
-            val authorTextView: TextView = findViewById(R.id.authorTextView)
-
+        binding.button.setOnClickListener {
             // In case of having the initial display, a change in displayed elements is required
-            if (initialTextView.isVisible) {
-                initialTextView.visibility = View.INVISIBLE
-                quoteTextView.visibility = View.VISIBLE
-                authorTextView.visibility = View.VISIBLE
+            if (binding.initialTextView.isVisible) {
+                binding.initialTextView.visibility = View.INVISIBLE
+                binding.quoteTextView.visibility = View.VISIBLE
+                binding.authorTextView.visibility = View.VISIBLE
             }
 
-            val currentQuote: QuotesQuery.Quote? = model.updateAndGetQuoteData().value
-
-            if (currentQuote != null) {
-                "\" ${currentQuote.quote}\"".also { quoteTextView.text = it }
-                "- ${currentQuote.author}".also { authorTextView.text = it }
+            model.updateAndGetQuoteData().observe(this) { currentQuote ->
+                "\" ${currentQuote.quote}\"".also { binding.quoteTextView.text = it }
+                "- ${currentQuote.author}".also { binding.authorTextView.text = it }
             }
         }
-
     }
 
     /**
@@ -54,14 +45,12 @@ class MainActivity : AppCompatActivity() {
      * and enables the click button.
      */
     private fun getQuotesQuery() {
-        val demotivateButton: Button = findViewById(R.id.button)
-
         this.lifecycleScope.launchWhenResumed {
             try {
                 val response = apolloClient.query(QuotesQuery()).execute()
                 response.data?.let { model.setQuotes(it.quotes) }
 
-                demotivateButton.isEnabled = true
+                binding.button.isEnabled = true
             } catch (exception: ApolloException) {
                 Log.d("QuotesList", "Unable to fetch quotes", exception)
             }
