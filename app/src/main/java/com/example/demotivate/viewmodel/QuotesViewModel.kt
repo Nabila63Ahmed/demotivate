c69f16182038cdfdd8fcdca294d36fac0229ac5a
Nabila63Ahmed <nabila.ahmed63@gmail.com>
2022-05-09 17:57:55 +0200

    :recycle: QuotesQuery.Quote -> Quote

--- app/src/main/java/com/example/demotivate/viewmodel/QuotesViewModel.kt
+++ app/src/main/java/com/example/demotivate/viewmodel/QuotesViewModel.kt
@@ -4,13 +4,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.demotivate.QuotesQuery
+import com.example.demotivate.QuotesQuery.Quote
 import com.example.demotivate.graphql.QuotesProvider
 import kotlinx.coroutines.launch
 
 class QuotesViewModel : ViewModel() {
-    private lateinit var quotes: List<QuotesQuery.Quote>
-    private val quoteData = MutableLiveData<QuotesQuery.Quote>()
+    private lateinit var quotes: List<Quote>
+    private val quoteData = MutableLiveData<Quote>()
 
     /**
      * Sets the ViewModel quotes to the fetched quotes by the Provider.
@@ -24,7 +24,7 @@ class QuotesViewModel : ViewModel() {
     /**
      * Picks a quote at random and updates the MutableLiveData.
      */
-    fun updateAndGetQuoteData(): LiveData<QuotesQuery.Quote> {
+    fun updateAndGetQuoteData(): LiveData<Quote> {
         quoteData.value = quotes.random()
 
         return quoteData
