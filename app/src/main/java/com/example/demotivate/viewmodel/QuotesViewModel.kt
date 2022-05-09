d746f90be504490adf841f5801fca5be20e8022f
Nabila63Ahmed <nabila.ahmed63@gmail.com>
2022-05-09 17:20:16 +0200

    :recycle: Random pick directly from Quotes array

--- app/src/main/java/com/example/demotivate/viewmodel/QuotesViewModel.kt
+++ app/src/main/java/com/example/demotivate/viewmodel/QuotesViewModel.kt
@@ -32,14 +32,8 @@ class QuotesViewModel : ViewModel() {
      * Picks a quote at random and updates the MutableLiveData.
      */
     fun updateAndGetQuoteData(): LiveData<QuotesQuery.Quote> {
-        val currentIndex = randomIndex(quotes.size)
-        quoteData.value = quotes[currentIndex]
+        quoteData.value = quotes.random()
 
         return quoteData
     }
 }
-
-/**
- * Given an array size, it returns a random number within the indices of the array.
- */
-fun randomIndex(size: Int) = (0 until size).random()
--- app/src/test/java/com/example/demotivate/ExampleUnitTest.kt
+++ app/src/test/java/com/example/demotivate/ExampleUnitTest.kt
@@ -1,18 +1,9 @@
 package com.example.demotivate
 
-import com.example.demotivate.viewmodel.randomIndex
-import org.junit.Assert.assertTrue
-import org.junit.Test
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-    @Test
-    fun randomNumber_inRange() {
-        val randomNumber = randomIndex(6)
-        assertTrue("The value of randomNumber was not between 1 and 6", randomNumber in 0 until 6)
-    }
 }
