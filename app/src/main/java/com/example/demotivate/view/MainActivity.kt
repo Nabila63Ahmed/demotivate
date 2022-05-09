e2e4739e838834788c7705088edaf23452251618
Nabila63Ahmed <nabila.ahmed63@gmail.com>
2022-05-09 17:13:59 +0200

    :recycle: Migrate the API query coroutine scope (View -> ViewModel)

--- app/src/main/java/com/example/demotivate/view/MainActivity.kt
+++ app/src/main/java/com/example/demotivate/view/MainActivity.kt
@@ -1,16 +1,11 @@
 package com.example.demotivate.view
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
-import androidx.lifecycle.lifecycleScope
-import com.apollographql.apollo3.exception.ApolloException
-import com.example.demotivate.QuotesQuery
 import com.example.demotivate.databinding.ActivityMainBinding
-import com.example.demotivate.graphql.apolloClient
 import com.example.demotivate.viewmodel.QuotesViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -23,7 +18,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.button.isEnabled = false
-        getQuotesQuery()
+        model.getQuotesFromAPI()
+        binding.button.isEnabled = true
 
         binding.button.setOnClickListener {
             // In case of having the initial display, a change in displayed elements is required
@@ -36,23 +32,6 @@ class MainActivity : AppCompatActivity() {
             model.updateAndGetQuoteData().observe(this) { currentQuote ->
                 "\" ${currentQuote.quote}\"".also { binding.quoteTextView.text = it }
                 "- ${currentQuote.author}".also { binding.authorTextView.text = it }
-            }
-        }
-    }
-
-    /**
-     * Uses the Apollo client to fetch all quotes, posts them to the ViewModel
-     * and enables the click button.
-     */
-    private fun getQuotesQuery() {
-        this.lifecycleScope.launchWhenResumed {
-            try {
-                val response = apolloClient.query(QuotesQuery()).execute()
-                response.data?.let { model.setQuotes(it.quotes) }
-
-                binding.button.isEnabled = true
-            } catch (exception: ApolloException) {
-                Log.d("QuotesList", "Unable to fetch quotes", exception)
             }
         }
     }
--- app/src/main/java/com/example/demotivate/viewmodel/QuotesViewModel.kt
+++ app/src/main/java/com/example/demotivate/viewmodel/QuotesViewModel.kt
@@ -1,19 +1,34 @@
 package com.example.demotivate.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.apollographql.apollo3.exception.ApolloException
 import com.example.demotivate.QuotesQuery
+import com.example.demotivate.graphql.apolloClient
+import kotlinx.coroutines.launch
 
 class QuotesViewModel : ViewModel() {
     private lateinit var quotes: List<QuotesQuery.Quote>
     private val quoteData = MutableLiveData<QuotesQuery.Quote>()
 
-    fun setQuotes(quotes: List<QuotesQuery.Quote>) {
-        this.quotes = quotes
+    /**
+     * Uses the Apollo client to fetch all quotes and sets them to the ViewModel quotes.
+     */
+    fun getQuotesFromAPI() {
+        viewModelScope.launch {
+            try {
+                val response = apolloClient.query(QuotesQuery()).execute()
+                response.data?.let { quotes = it.quotes }
+            } catch (exception: ApolloException) {
+                Log.d("QuotesList", "Unable to fetch quotes", exception)
+            }
+        }
     }
 
-    /*
+    /**
      * Picks a quote at random and updates the MutableLiveData.
      */
     fun updateAndGetQuoteData(): LiveData<QuotesQuery.Quote> {
@@ -24,7 +39,7 @@ class QuotesViewModel : ViewModel() {
     }
 }
 
-/*
-* Given an array size, it returns a random number within the indices of the array.
-*/
+/**
+ * Given an array size, it returns a random number within the indices of the array.
+ */
 fun randomIndex(size: Int) = (0 until size).random()
