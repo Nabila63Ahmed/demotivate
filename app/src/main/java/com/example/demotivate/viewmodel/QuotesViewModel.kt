155a49cb11183e66d952723778fad5dfcd31126d
Nabila63Ahmed <nabila.ahmed63@gmail.com>
2022-05-23 13:06:54 +0200

    :construction: Update the ViewModel with a quotes' repository

--- app/src/main/java/com/example/demotivate/viewmodel/QuotesViewModel.kt
+++ app/src/main/java/com/example/demotivate/viewmodel/QuotesViewModel.kt
@@ -4,14 +4,36 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.demotivate.QuotesQuery.Quote
+import com.example.demotivate.QuotesQuery
 import com.example.demotivate.Util
+import com.example.demotivate.data.db.entities.Quote
+import com.example.demotivate.data.repositories.QuotesRepository
 import com.example.demotivate.graphql.QuotesProvider
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class QuotesViewModel : ViewModel() {
-    private lateinit var quotes: List<Quote>
-    private val quoteData = MutableLiveData<Quote>()
+class QuotesViewModel(
+    private val repository: QuotesRepository
+) : ViewModel() {
+    private lateinit var quotes: List<QuotesQuery.Quote>
+    private val quoteData = MutableLiveData<QuotesQuery.Quote>()
+
+    fun insert(item: Quote) = CoroutineScope(Dispatchers.Main).launch {
+        repository.insert(item)
+    }
+
+    fun insertAll(items: List<Quote>) = CoroutineScope(Dispatchers.Main).launch {
+        repository.insertAll(items)
+    }
+
+    fun delete(item: Quote) = CoroutineScope(Dispatchers.Main).launch {
+        repository.delete(item)
+    }
+
+    fun getAllQuotes() = repository.getAllQuotes()
+    
+    fun getQuoteByID(id: Int) = repository.getQuoteByID(id)
 
     /**
      * Sets the ViewModel quotes to the fetched quotes by the Provider.
@@ -27,7 +49,7 @@ class QuotesViewModel : ViewModel() {
     /**
      * Picks a quote at random and updates the MutableLiveData.
      */
-    fun updateAndGetQuoteData(): LiveData<Quote> {
+    fun updateAndGetQuoteData(): LiveData<QuotesQuery.Quote> {
         quoteData.value = quotes[Util.generateRandomNumber(quotes.size)]
 
         return quoteData
