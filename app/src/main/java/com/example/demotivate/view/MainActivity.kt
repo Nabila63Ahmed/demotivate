e29d2b7dbe8ed7d3566e9d9f759727c76b7057c7
Nabila63Ahmed <nabila.ahmed63@gmail.com>
2022-05-23 18:28:32 +0200

    :construction: Use a viewModel factory and instantiate the viewModel in the main activity

--- app/src/main/java/com/example/demotivate/view/MainActivity.kt
+++ app/src/main/java/com/example/demotivate/view/MainActivity.kt
@@ -4,18 +4,25 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.demotivate.R
+import com.example.demotivate.data.db.QuotesDatabase
+import com.example.demotivate.data.repositories.QuotesRepository
 import com.example.demotivate.databinding.ActivityMainBinding
 import com.example.demotivate.viewmodel.QuotesViewModel
+import com.example.demotivate.viewmodel.QuotesViewModelFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val model: QuotesViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val db = QuotesDatabase(this)
+        val repository = QuotesRepository(db)
+        val factory = QuotesViewModelFactory(repository)
+
+        val model: QuotesViewModel by viewModels { factory }
         val initialFragment = InitialFragment()
         val quoteFragment = QuoteFragment()
 
